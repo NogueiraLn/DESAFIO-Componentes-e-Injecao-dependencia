38c57c3c16a18dcef6587bae804ae78c5d90d8ef
Nog <lucas.nogueiramartins@gmail.com>
2024-03-01 00:31:41 -0300

    initial commit

---
+++ src/main/java/com/desafio/Desafio/Dependencia/DesafioDependenciaApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.Desafio.Dependencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDependenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDependenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/Desafio/Dependencia/DesafioDependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.Desafio.Dependencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
