a2c7f132fc1c3701be5a39bc784f79e0810c8fdb
Nog <lucas.nogueiramartins@gmail.com>
2024-03-08 21:13:43 -0300

    Final test

--- src/main/java/com/desafio/Desafio/Dependencia/DesafioDependenciaApplication.java
+++ src/main/java/com/desafio/Desafio/Dependencia/DesafioDependenciaApplication.java
@@ -1,13 +1,30 @@
 package com.desafio.Desafio.Dependencia;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.desafio.Desafio.Dependencia.entities.Order;
+import com.desafio.Desafio.Dependencia.services.OrderService;
+
 @SpringBootApplication
-public class DesafioDependenciaApplication {
+public class DesafioDependenciaApplication implements CommandLineRunner {
+
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioDependenciaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Order order = new Order(1309, 95.90, 0.0);
+
+		System.out.println("Pedido código " + order.getCode());
+		System.out.println("Valor total: " + String.format("%.2f", orderService.total(order)));
+
+	}
 }
